25310bb2f4a1329db706a8451d6bbf6c6bcbcfd1
Vaibhav Vats <vats88690@gmail.com>
2025-10-05 17:15:55 +0530

    Include dist folder for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   base: '/Billing/',
   server: {
     host: '0.0.0.0',
     port: 5173, // or whatever port you're using
